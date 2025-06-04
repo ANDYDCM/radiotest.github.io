5a0b826012907936cc68c54507194a0008356619
ANDYDCM <andydariocastellanos@gmail.com>
2025-06-04 18:52:01 -0500

    conf

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", (e) => {
-  console.log("Instalando Service Worker...");
-  e.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener("activate", (e) => {
-  console.log("Service Worker activado.");
-  e.waitUntil(self.clients.claim());
-});
